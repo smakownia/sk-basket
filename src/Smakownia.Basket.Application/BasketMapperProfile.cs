c11530be85e0fd11ecde2eb56dfdbba81d9d3214
aruzo <dawid12391111@gmail.com>
2023-08-02 17:37:57 +0200

    add mapping

--- src/Smakownia.Basket.Application/BasketMapperProfile.cs
+++ src/Smakownia.Basket.Application/BasketMapperProfile.cs
@@ -8,8 +8,16 @@ public class BasketMapperProfile : Profile
 {
     public BasketMapperProfile()
     {
-        CreateMap<BasketEntity, BasketDto>();
-        CreateMap<BasketItem, BasketItemDto>();
+        CreateMap<BasketEntity, BasketDto>()
+            .ForMember(dest => dest.TotalItems,
+                       opt => opt.MapFrom(src => src.Items.Aggregate(0, (t, i) => t + i.Quantity)))
+            .ForMember(dest => dest.TotalPrice,
+                       opt => opt.MapFrom(src => src.Items.Aggregate(0L, (t, i) => t + i.Price * i.Quantity)));
+
+        CreateMap<BasketItem, BasketItemDto>()
+            .ForMember(dest => dest.TotalPrice,
+                       opt => opt.MapFrom(src => src.Price * src.Quantity));
+
         CreateMap<long, PriceDto>()
             .ConvertUsing(f => new() { Raw = f, Formatted = $"{decimal.Divide(f, 100)}zł" });
     }
--- src/Smakownia.Basket.Application/Dtos/BasketDto.cs
+++ src/Smakownia.Basket.Application/Dtos/BasketDto.cs
@@ -5,4 +5,5 @@ public class BasketDto
     public Guid Id { get; set; }
     public IEnumerable<BasketItemDto> Items { get; set; } = new List<BasketItemDto>();
     public PriceDto TotalPrice { get; set; } = new();
+    public int TotalItems { get; set; }
 }
