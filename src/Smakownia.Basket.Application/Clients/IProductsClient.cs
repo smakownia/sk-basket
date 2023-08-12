accf7b08be7664abf432573b89b9c369f5dc813b
Dawid Ostrzyżek <64381826+aruzo1@users.noreply.github.com>
2023-08-12 11:15:01 +0200

    Feature/30 add gateway responsibility to fetch product before adding item to basket (#31)
    * update command handler
    
    * remove unnecessary logic

--- src/Smakownia.Basket.Application/Clients/IProductsClient.cs
+++
@@ -1,8 +0,0 @@
-﻿using Smakownia.Basket.Application.Models;
-
-namespace Smakownia.Basket.Application.Clients;
-
-public interface IProductsClient
-{
-    Task<Product> GetByIdAsync(Guid id, CancellationToken cancellationToken);
-}
--- src/Smakownia.Basket.Application/Commands/AddBasketItem/AddBasketItemCommand.cs
+++ src/Smakownia.Basket.Application/Commands/AddBasketItem/AddBasketItemCommand.cs
@@ -1,21 +1,11 @@
 ﻿using MediatR;
 using Smakownia.Basket.Application.Dtos;
-using System.ComponentModel.DataAnnotations;
 
 namespace Smakownia.Basket.Application.Commands.AddBasketItem;
 
-public sealed record AddBasketItemCommand : IRequest<BasketDto>
-{
-    public AddBasketItemCommand(Guid id, int quantity)
-    {
-        Id = id;
-        Quantity = quantity;
-    }
-
-    [Required]
-    public Guid Id { get; init; }
-
-    [Required]
-    [Range(1, 999)]
-    public int Quantity { get; init; }
-}
+public sealed record AddBasketItemCommand(Guid Id,
+                                          string ImageUrl,
+                                          string Name,
+                                          string? Description,
+                                          long Price,
+                                          int Quantity) : IRequest<BasketDto>;
