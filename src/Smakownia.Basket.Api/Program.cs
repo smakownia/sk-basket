002221b19491d41a53b3958ba1dd01e267cf2463
Dawid Ostrzyżek <64381826+aruzo1@users.noreply.github.com>
2023-08-16 14:29:13 +0200

    Feature/38 add getting basket by identity (#39)
    * remove unused files
    
    * add getting basket by identity id

--- src/Smakownia.Basket.Api/Program.cs
+++ src/Smakownia.Basket.Api/Program.cs
@@ -1,15 +1,45 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Smakownia.Api.Middlewares;
 using Smakownia.Basket.Api.Services;
 using Smakownia.Basket.Application.Services;
 using Smakownia.Products.Application;
 using Smakownia.Products.Infrastructure;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices(builder.Configuration);
 
+builder.Services.AddAuthentication(opt =>
+{
+    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(options =>
+    {
+        options.Events = new JwtBearerEvents
+        {
+            OnMessageReceived = context =>
+            {
+                context.Token = context.Request.Cookies["Authorization"];
+                return Task.CompletedTask;
+            }
+        };
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["SecretKey"]))
+        };
+    });
+
 builder.Services.AddTransient<IBasketIdentityService, BasketIdentityService>();
+builder.Services.AddTransient<IIdentityService, IdentityService>();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -25,8 +55,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseMiddleware<GlobalExceptionMiddleware>();
 
-app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
